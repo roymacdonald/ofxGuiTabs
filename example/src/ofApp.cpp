7228453c2b9421756f7fb5d23732d4059ee3bb0e
Roy Macdonald <macdonald.roy@gmail.com>
2022-11-19 00:56:40 -0300

    Updated example

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -8,22 +8,46 @@ void ofApp::setup(){
 
     tabs.setup("Tabs");
     
-    for(int i = 0; i < 15; i++){
-        auto group = tabs.newTab("int  " + ofToString(i));
-        size_t n = round(ofRandom(2, 5));
-        for(int p = 0; p < n; p++){
-            params.push_back(ofParameter<float>(ofToString(i)+ "-" + ofToString(p), ofRandom(1), 0,1));
-            if(group) group->add(params.back());
-        }
-    }
+    //uncomment the following code to generate parameters and tabs via code
+//    for(int i = 0; i < 15; i++){
+//        auto group = tabs.newTab("int  " + ofToString(i));
+//        size_t n = round(ofRandom(2, 5));
+//        for(int p = 0; p < n; p++){
+//            params.push_back(ofParameter<float>(ofToString(i)+ "-" + ofToString(p), ofRandom(1), 0,1));
+//            if(group) group->add(params.back());
+//        }
+//    }
     
+    
+    auto tab1 = tabs.newTab("First tab");
+    
+    tab1->add(param_1_1);
+    tab1->add(param_1_2);
+    tab1->add(param_1_3);
+    tab1->add(param_1_4);
+
+    auto tab2 = tabs.newTab("Second tab");
+    tab2->add(param_2_1);
+    tab2->add(param_2_2);
+    tab2->add(param_2_3);
+    tab2->add(param_2_4);
+
+    auto tab3 = tabs.newTab("Tab Tres");
+    tab3->add(param_3_1);
+    tab3->add(param_3_2);
+    tab3->add(param_3_3);
+    tab3->add(param_3_4);
+
+    
+    /// in case you might want to know when a tab has been selected you can listen to the selection_E event
     listener = tabs.selection_E.newListener([](string& s){
         cout << "Tab changed " << s << endl;
     });
     
+    
     gui.add(&tabs);
 
-    tabs.setSelectedTab(tabs.getTabNames()[0]);
+    
     
 }
 
--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -24,14 +24,41 @@ class ofApp : public ofBaseApp{
 	
 	
 	ofxPanel gui;
-//    unique_ptr<ofxGuiTabs> tabs;
+
     ofxGuiTabs tabs;
     
     ofEventListener listener;
+
+    //uncomment the following line to algorithmically fill the tabs and guis
+    // FYI: it is a lot more cryptic!
+    //vector<ofParameter<float>> params;
     
-    vector<ofParameter<float>> params;
+
+    /// just a bunch of parameter to fill up the gui tabs
+
+    // for tab 1
+    ofParameter<float> param_1_1 = {"param_1_1", 0, 0, 1};
+    ofParameter<float> param_1_2 = {"param_1_2", 0.33, 0, 1};
+    ofParameter<float> param_1_3 = {"param_1_3", 0.66, 0, 1};
+    ofParameter<float> param_1_4 = {"param_1_4", 1, 0, 1};
+
+
+	// for tab 2
+    ofParameter<float> param_2_1 = {"param_2_1", 1, 0, 1};
+	ofParameter<float> param_2_2 = {"param_2_2", 0, 0, 1};
+	ofParameter<float> param_2_3 = {"param_2_3", 0.33, 0, 1};
+	ofParameter<float> param_2_4 = {"param_2_4", 0.66, 0, 1};
+
+
+	// for tab 3
+    ofParameter<float> param_3_1 = {"param_3_1", 0.33, 0, 1};
+	ofParameter<float> param_3_2 = {"param_3_2", 0.66, 0, 1};
+	ofParameter<float> param_3_3 = {"param_3_3", 1, 0, 1};
+	ofParameter<float> param_3_4 = {"param_3_4", 0, 0, 1};
+
+
     
-    
+
     
 	
 };
